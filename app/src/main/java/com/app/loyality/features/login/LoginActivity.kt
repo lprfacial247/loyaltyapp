16eb74f1d14d93bd88684fd4d0e5e4c64beb8067
masum <masum.abir43@gmail.com>
2024-03-01 00:57:23 +0600

    scan option init

--- app/src/main/java/com/app/loyality/features/login/LoginActivity.kt
+++ app/src/main/java/com/app/loyality/features/login/LoginActivity.kt
@@ -3,10 +3,20 @@ package com.app.loyality.features.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.app.loyality.R
+import com.app.loyality.common.extensions.changeActivity
+import com.app.loyality.databinding.ActivityLoginBinding
+import com.app.loyality.features.main.MainActivity
+import com.app.loyality.features.scanOption.ScanOptionsActivity
+import com.wada811.viewbinding.viewBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(R.layout.activity_login) {
+    private val binding by viewBinding(ActivityLoginBinding::bind)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding.btnGetStart.setOnClickListener {
+            changeActivity(ScanOptionsActivity::class.java)
+        }
     }
 }
---
+++ app/src/main/java/com/app/loyality/features/scanOption/ScanOptionsActivity.kt
@@ -0,0 +1,12 @@
+package com.app.loyality.features.scanOption
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.loyality.R
+
+class ScanOptionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_options)
+    }
+}
