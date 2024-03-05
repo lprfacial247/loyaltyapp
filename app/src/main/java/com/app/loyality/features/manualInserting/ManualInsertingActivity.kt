02511c57a1b710438ca3918a3fa7ac5c34cd642b
masum <masum.abir43@gmail.com>
2024-03-06 00:37:38 +0600

    design

---
+++ app/src/main/java/com/app/loyality/features/manualInserting/ManualInsertingActivity.kt
@@ -0,0 +1,12 @@
+package com.app.loyality.features.manualInserting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.loyality.R
+
+class ManualInsertingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manual_inserting)
+    }
+}
